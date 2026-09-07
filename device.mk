561811089e11c7cbd23d235390ae4040fee98940
Marc Bourgoin <themard85@gmail.com>
2026-09-06 22:41:24 -0600

    tundra: Add e & n manifest SKUs
    e is eSIM with a single nano-SIM
    
    n is single nano-SIM
    
    We don't need to include de & dn SKUs since the only differences there are secure_element,
    and our common tree doesn't include it
    
    Change-Id: Ieae9d0935c4d411ae01a112f8183d0ab6767a0e1

--- device.mk
+++ device.mk
@@ -95,5 +95,12 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set, MOTOROLA_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touchscreen/primary/interpolation)
 
+# VINTF
+ODM_MANIFEST_SKUS += e n
+ODM_MANIFEST_E_FILES := \
+    device/motorola/sm7325-common/vintf/manifest_ss.xml
+ODM_MANIFEST_N_FILES := \
+    device/motorola/sm7325-common/vintf/manifest_ss.xml
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/tundra/tundra-vendor.mk)
